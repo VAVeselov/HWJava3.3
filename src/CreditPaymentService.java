79406a5211d427b44fad92b9770664467dff31d3
Влад Веселов <veselo.vlad9976@gmail.com>
2023-08-11 12:17:02 +0300

    Bug Fix #2

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
-    public double calculate(double month, double summa, double persent) {
+    public double calculate(double month, double sum, double persent) {
         double persentInMonth = persent / 12 / 100;
         double annuitRatio = (persentInMonth * Math.pow((1 + persentInMonth), month)) / (Math.pow((1 + persentInMonth), month) - 1);
-        double platesh = summa * annuitRatio;
+        double paiment = sum * annuitRatio;
 
-        return platesh;
+        return paiment;
     }
 }
